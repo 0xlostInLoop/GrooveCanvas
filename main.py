09afece385950d286634fc1a022765f6ea74466f
0xlostInLoop <direxpvp@gmail.com>
2025-06-07 05:24:58 +0200

    changed port

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ def analysis_file(filename):
     return send_file(filepath, mimetype=mimetype)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
